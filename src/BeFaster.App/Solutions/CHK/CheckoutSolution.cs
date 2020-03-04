20fd5a8357af97956bbf06cb3767de9bd005cfe1
sbx_user1051 <sbx_user1051@169.254.138.93>
2020-03-04 01:00:27 +0000

    Wed Mar 04 01:00:27 UTC 2020

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -13,7 +13,7 @@ namespace BeFaster.App.Solutions.CHK
             {
                 return 0;
             }
-            if (!Regex.IsMatch(skus, @"^[A-F]+$"))
+            if (!Regex.IsMatch(skus, @"^[A-Z]+$"))
             {
                 return -1;
             }
@@ -23,6 +23,26 @@ namespace BeFaster.App.Solutions.CHK
             int D_count = skus.Count(v => v == 'D');
             int E_count = skus.Count(v => v == 'E');
             int F_count = skus.Count(v => v == 'F');
+            int G_count = skus.Count(v => v == 'G');
+            int H_count = skus.Count(v => v == 'H');
+            int I_count = skus.Count(v => v == 'I');
+            int J_count = skus.Count(v => v == 'J');
+            int K_count = skus.Count(v => v == 'K');
+            int L_count = skus.Count(v => v == 'L');
+            int M_count = skus.Count(v => v == 'M');
+            int N_count = skus.Count(v => v == 'N');
+            int O_count = skus.Count(v => v == 'O');
+            int P_count = skus.Count(v => v == 'P');
+            int Q_count = skus.Count(v => v == 'Q');
+            int R_count = skus.Count(v => v == 'R');
+            int S_count = skus.Count(v => v == 'S');
+            int T_count = skus.Count(v => v == 'T');
+            int U_count = skus.Count(v => v == 'U');
+            int V_count = skus.Count(v => v == 'V');
+            int W_count = skus.Count(v => v == 'W');
+            int X_count = skus.Count(v => v == 'X');
+            int Y_count = skus.Count(v => v == 'Y');
+            int Z_count = skus.Count(v => v == 'Z');
             if (B_count >= (E_count / 2))
             { 
                 B_count = B_count - (E_count / 2);
@@ -44,6 +64,7 @@ namespace BeFaster.App.Solutions.CHK
         }
     }
 }
+
 
 
 
